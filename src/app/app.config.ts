3c149f4fc59ad7ce773d9389ad2a3f1a63543167
Caio Camargo <caiocbahia@hotmail.com>
2025-06-15 17:30:56 -0300

    feat: first version of spinner

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { loadingInterceptor } from './interceptors/loading-interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(),
+    provideHttpClient(withInterceptors([loadingInterceptor])),
+    importProvidersFrom([BrowserAnimationsModule])
   ]
 };
