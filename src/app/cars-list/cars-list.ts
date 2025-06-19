acfa222a1cbfcc10d6a4a438ec558eef0614c95f
Caio Camargo <caiocbahia@hotmail.com>
2025-06-18 21:14:10 -0300

    feat: added car availability property on car card

--- src/app/cars-list/cars-list.ts
+++ src/app/cars-list/cars-list.ts
@@ -11,5 +11,32 @@ import { NgxSpinnerModule } from 'ngx-spinner';
 export class CarsList {
 
   @Input() carsList: any
+  @Input() recordsList: any
 
+  recordPlatesArr: any[] = []
+
+  statusClass:string = ''
+
+  checkAvailable() {
+
+    //esse for é apenas pra criar um array com as placas dos carros alugados
+    for (let index = 0; index < this.recordsList.length; index++) {
+      const element = this.recordsList[index].plate
+      this.recordPlatesArr.push(element)
+    }
+
+    for (let index = 0; index < this.carsList.length; index++) {
+      const carlistPlate = this.carsList[index].licensePlate
+
+      if(this.recordPlatesArr.includes(carlistPlate)){
+        this.carsList[index].status = 'Alugado'
+
+      } else {
+        this.carsList[index].status = 'Disponível'
+
+      }
+    }
+    console.log(this.recordPlatesArr)
+
+  }
 }
